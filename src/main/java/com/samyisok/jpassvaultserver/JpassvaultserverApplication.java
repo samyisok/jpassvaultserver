0c897e5c0a237e7cdfbea7b2ff7e4e143179bb25
Sergey M <magaster@gmail.com>
2021-04-13 13:12:30 +0300

    first init

---
+++ src/main/java/com/samyisok/jpassvaultserver/JpassvaultserverApplication.java
@@ -0,0 +1,13 @@
+package com.samyisok.jpassvaultserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpassvaultserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpassvaultserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samyisok/jpassvaultserver/JpassvaultserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samyisok.jpassvaultserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpassvaultserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
