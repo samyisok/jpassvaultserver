98f8223bfd343b30399d439dfdb333b56a394fd0
Sergey M <magaster@gmail.com>
2021-04-17 00:36:54 +0300

    fix verify test

--- src/test/java/com/samyisok/jpassvaultserver/auth/AuthCheckVerifyTest.java
+++ src/test/java/com/samyisok/jpassvaultserver/auth/AuthCheckVerifyTest.java
@@ -4,36 +4,30 @@ package com.samyisok.jpassvaultserver.auth;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import com.samyisok.jpassvaultserver.AppProperties;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 @SpringBootTest
 class AuthCheckVerifyTest {
-
-  @MockBean
-  private AppProperties appProperties;
-
-  @Autowired
+  @SpyBean
   private AuthCheck authCheck;
 
   private String envToken = "env-token";
-  private String localToken = "local-token";
 
   @BeforeEach
   public void setUp() {
-    when(appProperties.getUseSecretKeyFromEnv()).thenReturn(true);
-    when(appProperties.getEnvSecretKey()).thenReturn(envToken);
-    when(appProperties.getSecretKey()).thenReturn(localToken);
-
+    reset();
+    when(authCheck.getKey()).thenReturn(envToken);
   }
 
   @Test
-  void verify() {
+  void verifyShouldBeTrue() {
     assertTrue(authCheck.verify(envToken));
   }
 
@@ -45,7 +39,8 @@ class AuthCheckVerifyTest {
 
   @Test
   void verifyShouldThrowException() {
-    when(appProperties.getEnvSecretKey()).thenReturn(null);
+    when(authCheck.getKey()).thenReturn(null);
     assertThrows(RuntimeException.class, () -> authCheck.verify(envToken));
+    verify(authCheck, times(1)).getKey();
   }
 }
