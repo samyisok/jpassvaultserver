29282ef9c1df21f1aa2435fe26e188ab34da5448
Sergey M <magaster@gmail.com>
2021-05-07 21:56:28 +0300

    add checksum

--- src/main/java/com/samyisok/jpassvaultserver/controllers/FileController.java
+++ src/main/java/com/samyisok/jpassvaultserver/controllers/FileController.java
@@ -2,7 +2,10 @@ package com.samyisok.jpassvaultserver.controllers;
 
 
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
+import javax.xml.bind.DatatypeConverter;
 import com.samyisok.jpassvaultserver.domains.File;
 import com.samyisok.jpassvaultserver.domains.FileNotFoundException;
 import com.samyisok.jpassvaultserver.domains.FileRepository;
@@ -25,6 +28,32 @@ class FileController {
   @GetMapping("/check")
   Map<String, String> check() {
     return Map.of("check", "ok");
+  }
+
+  @GetMapping("/files/last/checksum")
+  Map<String, String> checksum() {
+    File file =
+        repository.findFirst1ByOrderByIdDesc().stream().findFirst().orElse(null);
+
+    if (file == null) {
+      return Map.of("hash", "");
+    }
+
+    String fileDb = file.getFile();
+    MessageDigest md;
+    try {
+      md = MessageDigest.getInstance("MD5");
+      md.update(fileDb.getBytes());
+      byte[] digest = md.digest();
+      String checksum = DatatypeConverter
+          .printHexBinary(digest).toUpperCase();
+
+      return Map.of("hash", checksum);
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+
+    return Map.of("hash", null);
   }
 
   @PostMapping("/files")
